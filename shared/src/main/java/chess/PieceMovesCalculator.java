1b3760161a078b05bb9eb40771f483a9fcf15e71
Marissa Stephens <rissa.step6@gmail.com>
2026-09-14 11:42:06 -0600

    Changed PieceMovesCalculator to an interface

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,6 +1,7 @@
 package chess;
 
-public class PieceMovesCalculator {
-    //logic for if given a certain piece implement that piece's class moves
-    //pieceMoves(board: ChessBoard, position: ChessPosition): Collection<ChessMoves>
+import java.util.Collection;
+
+public interface PieceMovesCalculator {
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
 }
